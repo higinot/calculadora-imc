5508c196fede647306a59eab52c8aeaa03e96e3c
higinot <engprodhigino@gmail.com>
2022-04-27 10:49:05 -0300

    Calculadora de IMC Completa

--- js/script.js
+++ js/script.js
@@ -3,25 +3,59 @@ const form = document.querySelector(".form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const inputPeso = document.querySelector('#peso');
-    const inputAltura = document.querySelector('#altura');
+  const inputPeso = document.querySelector("#peso");
+  const inputAltura = document.querySelector("#altura");
 
   setResultado();
-
 });
 
-const resultado = document.createElement("p");
+const resultado = document.createElement("h3");
 
+function imc(peso, altura) {
+  const imc = Number(peso) / Number(Number(altura) ** 2);
+  imc.toFixed(2);
+
+  const result = [
+    "Abaixo do peso",
+    "Peso normal",
+    "Sobrepeso",
+    "Obesidade grau 1",
+    "Obesidade grau 2",
+    "Obesidade grau 3",
+  ];
+
+  if (imc >= 39.9) return result[5];
+  if (imc >= 34.9) return result[4];
+  if (imc >= 29.9 ) return result[3];
+  if (imc >= 24.9) return result[2];
+  if (imc >= 18.5) return result[1];
+  if (imc < 18.5) return result[0];
+
+}
 
 function setResultado() {
-  resultado.innerHTML = ''; //Zerando o html
-  resultado.className = "resultado";
+  resultado.innerHTML = ""; //Zerando o html
+  resultado.className = "imc";
 
-  if(!inputPeso) {
-      console.log('ojjj')
-  }
+  const inputPeso = document.querySelector("#peso");
+  const inputAltura = document.querySelector("#altura");
+  const calculo = Number(inputPeso.value) + Number(inputAltura.value);
 
   form.appendChild(resultado);
-  const result = Number(inputAltura.value) + Number(inputPeso.value)
-  return resultado.innerHTML = `${result}`
+
+  if (!inputAltura.value && !inputPeso.value) {
+    return (resultado.innerHTML = "Digite o peso e a altura!");
+  }
+  if (!inputAltura.value) {
+    return (resultado.innerHTML = "Você precisa digitar a Altura!");
+  }
+  if (!inputPeso.value) {
+    return (resultado.innerHTML = "Você precisa digitar o Peso!");
+  }
+
+  if (inputAltura.value && inputPeso.value) {
+    return (resultado.innerHTML = imc(inputPeso.value, inputAltura.value));
+  }
+
+  console.log(inputAltura, inputAltura);
 }
